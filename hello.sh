2049f57ab9b5b57d53dfb709d1394ba73707d341
Mahesh8120 <jonnalagadamahi@gmail.com>
2025-09-19 12:54:25 +0530

    shell

--- hello.sh
+++ hello.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
+$name1="mahesh"
+$name2="gopi"
+echo  $name1::"hello $name2 how are you?
+echo  $name2:: I am fine $name2"
 
-# Assign values to variables (not to $1, $2 directly)
-name1="mahesh"
-name2="gopi"
-
-# Corrected echo statements with proper quotes and syntax
-echo "$name1:: hello $name2, how are you?"
-echo "$name2:: I am fine, $name1"
 
