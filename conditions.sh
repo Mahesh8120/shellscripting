aa3d68b75fff4ce986fcafc4be3923cc3c8437ea
Mahesh8120 <jonnalagadamahi@gmail.com>
2025-09-23 10:55:36 +0530

    shell

---
+++ conditions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+number=$!
+
+if [$number le 15]; then
+  echo "number is less than or equal to 15"
+else
+  echo "number is greater than 15"
+fi
+
+userid=$(id -u)
+
+if ($userid - ne 0); then 
+  echo "error: run the script with root"
+  exit 1
+else
+  echo "you are root user"
+fi
+
+if ($? -ne 0); then
--- environment.sh
+++ environment.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo hi I am learning $course
+
+# using "export" we can set environment variable -- temporary
+# ".bashrc" to set environment variable permanently 
