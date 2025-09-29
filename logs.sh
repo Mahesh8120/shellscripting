fa63a970b054bef1c8f7ef5b1353b9cab49db976
Mahesh8120 <jonnalagadamahi@gmail.com>
2025-09-29 11:28:24 +0530

    logs

--- logs.sh
+++ logs.sh
@@ -15,16 +15,14 @@ fi
 
 mkdir -p /var/log/shell-script
  
-
-validate() [
+ validate() {
    if [ $1 -ne 0 ]; then
      echo -e $r"$2 install failed"$n
      exit 1
    else 
      echo -e $g"$2 installed"$n
    fi 
-]
-
+ }
 dnf list installed nginx
 if [ $? -ne 0 ]; then 
    dnf install nginx -y
