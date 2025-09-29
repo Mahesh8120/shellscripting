cfac93613125b111f653fba9951666a5e776abf0
Mahesh8120 <jonnalagadamahi@gmail.com>
2025-09-29 12:12:07 +0530

    logs

--- logs.sh
+++ logs.sh
@@ -8,6 +8,7 @@ n='\e[0m'
 log_folder="var/log/shell-script"
 script_name=$( echo $0 | cut -d "." -f1 )
 log_file="$log_folder/$script_name.log"
+mkdir -p $log_folder
 
 if [ $userid -ne 0 ]; then
   echo -e $r"error: run with root access"$n
@@ -17,7 +18,6 @@ else
 fi
 echo "script started executing at: $(date)"
 
- mkdir -p $log_folder
  validate() {
    if [ $1 -ne 0 ]; then
      echo -e $r"$2 install failed"$n
@@ -26,9 +26,9 @@ echo "script started executing at: $(date)"
      echo -e $g"$2 installed"$n
    fi 
  }
-dnf list installed nginx 
+dnf list installed nginx &>>$log_file
 if [ $? -ne 0 ]; then 
-   dnf install nginx -y &>> $log_file
+   dnf install nginx -y &>>$log_file
    validate $? nginx
 else 
    echo -e $y"nginx already installed"$n
@@ -36,7 +36,7 @@ fi
 
 dnf list installed mysql
 if [ $? -ne 0 ]; then 
-  dnf install mysql -y &>> $log_file
+  dnf install mysql -y &>>$log_file
   validate $? mysql
 else 
    echo -e $y"nginx already installed"$n
