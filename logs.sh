12adb89660dbb83db0ac41bc03dabb6203d8459a
Mahesh8120 <jonnalagadamahi@gmail.com>
2025-09-29 11:46:17 +0530

    logs

--- logs.sh
+++ logs.sh
@@ -6,7 +6,7 @@ g='\e[32m'
 y='\e[33m'
 n='\e[0m'
 log_folder="var/log/shell-script"
-script_name="$(echo $0 | cut -d "." -fl)"
+script_name="$(echo $0 | cut -d "." -f1)"
 log_file="$log_folder/$script_name.log"
 
 
